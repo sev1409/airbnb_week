f8b90c84732d90804f407f27f2901efcd6ac8505
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-07 10:46:03 -0500

    generated all the controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,42 @@
 Rails.application.routes.draw do
+  get 'reviews/show'
+
+  get 'reviews/create'
+
+  get 'bookings/show'
+
+  get 'bookings/create'
+
+  get 'bookings/update'
+
+  get 'puppies/index'
+
+  get 'puppies/show'
+
+  get 'puppies/new'
+
+  get 'puppies/create'
+
+  get 'puppies/edit'
+
+  get 'puppies/update'
+
+  get 'puppies/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   devise_for :users
   root to: 'pages#home'
   resources :puppies do
