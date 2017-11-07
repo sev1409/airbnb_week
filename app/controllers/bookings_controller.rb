69b9c175d749797298b394363b8ca008c97b7166
to <osmont.tom@gmail.com>
2017-11-07 18:16:02 -0500

    booking controller and show view for one puppy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,25 @@
 class BookingsController < ApplicationController
+  before_action :set_puppy
+
   def show
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.puppy = @puppy
+    @booking.save!
   end
 
   def update
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :user_id, :puppy_id)
+  end
+
+  def set_puppy
+    @booking = Booking.find(params[:puppy_id])
   end
 end
