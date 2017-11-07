f3c8a219fa14b93832592cc8a79d753648c9b9cf
to <osmont.tom@gmail.com>
2017-11-07 15:30:30 -0500

    seed added for testing

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -3,8 +3,8 @@ class Puppy < ApplicationRecord
   has_many :reviews
   has_many :users, through: :bookings
 
-  # validates :name, presence: true
-  # validates :address, presence: true
-  # validates :breed, presence: true
-  # validates :description, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :breed, presence: true
+  validates :description, presence: true
 end
